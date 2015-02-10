8ab03b01f0a07c3ab876331644b5e39c616887c7
WemyJu <wemyju@gmail.com>
2015-02-10 12:15:04 +0800

    use UITabBarController to implement 4 main pages

---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KaohsiungMRTGO
+//
+//  Created by MW on 2015/2/10.
+//  Copyright (c) 2015年 Makris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITabBarController
+
+@end
